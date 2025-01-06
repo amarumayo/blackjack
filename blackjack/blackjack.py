577437acc2401fb0c099fcf87eee9a04d2ac0729
amarumayo <amarumayo@gmail.com>
2025-01-05 19:46:42 -0700

    removing duplicate test code

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -82,24 +82,6 @@ deck.cards[1]
 
 
 
-# test Hand.has_blackjack method
-# test that a black jack is detected
-player = Hand(is_dealer = False)    
-player.cards = [Card("A", "Spades"), Card("10", "Clubs")]
-player.value
-player.has_blackjack()
-
-# test that a black jack is not detected with 3 cards that equal 21
-player = Hand(is_dealer = False)    
-player.cards = [Card("A", "Spades"), Card("8", "Clubs"), Card("2", "clubs")]
-player.value
-player.has_blackjack()
-
-# test that a black jack is not detected with 2 cards that do not equal 21
-player = Hand(is_dealer = False)    
-player.cards = [Card("A", "Spades"), Card("8", "Clubs")]
-player.value
-player.has_blackjack()
 
 
 dealer = Hand(is_dealer = True)
